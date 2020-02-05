996b498b9f84e4ded30581331d0d86c89c2cbd5d
tursynbekoff <talgat.tursynbekov@gmail.com>
2020-02-05 11:51:35 +0100

    adding dependencies and installing them

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require(`path`);
+
+module.exports = {
+    mode: `development`,
+    entry: `./src/index.js`,
+    output: {
+      filename: `bundle.js`,
+      path: path.join(__dirname, `public`)
+    },
+    devServer: {
+      contentBase: path.join(__dirname, `public`),
+      open: false,
+      compress: true,
+      watchContentBase: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: `babel-loader`,
+          },
+        }
+      ],
+    },
+    devtool: `source-map`,
+  };
