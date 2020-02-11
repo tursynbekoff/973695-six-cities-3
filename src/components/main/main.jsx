279c562dd696fe0ee2b394c2f269aea98c3951ff
tursynbekoff <talgat.tursynbekov@gmail.com>
2020-02-11 14:13:14 +0100

    adding prop-types

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-
-const Main = (props) => {
+const Main = ({rentOptionsCount, rentOptionsDescriptions}) => {
   // eslint-disable-next-line react/prop-types
-  const {rentOptionsCount} = props;
 
   return (
     <div className="page page--gray page--main">
@@ -84,18 +83,44 @@ const Main = (props) => {
 
               </form>
               <div className="cities__places-list places__list tabs__content">
-                <article className="cities__place-card place-card">
-                  <div className="place-card__mark">
-                    <span>Premium</span>
-                  </div>
-                  <div className="cities__image-wrapper place-card__image-wrapper">
-                    <a href="#">
-                      <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
-                    </a>
-                  </div>
 
-                </article>
+                {rentOptionsDescriptions.map((rentOptionsDescription, index) => (
+                  <article className="cities__place-card place-card" key={index}>
+                    <div className="place-card__mark">
+                      <span>Premium</span>
+                    </div>
+                    <div className="cities__image-wrapper place-card__image-wrapper">
+                      <a href="#">
+                        <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+                      </a>
+                    </div>
+                    <div className="place-card__info">
+                      <div className="place-card__price-wrapper">
+                        <div className="place-card__price">
+                          <b className="place-card__price-value">&euro;120</b>
+                          <span className="place-card__price-text">&#47;&nbsp;night</span>
+                        </div>
+                        <button className="place-card__bookmark-button button" type="button">
+                          <svg className="place-card__bookmark-icon" width="18" height="19">
 
+                          </svg>
+                          <span className="visually-hidden">To bookmarks</span>
+                        </button>
+                      </div>
+                      <div className="place-card__rating rating">
+                        <div className="place-card__stars rating__stars">
+
+                          <span className="visually-hidden">Rating</span>
+                        </div>
+                      </div>
+                      <h2 className="place-card__name">
+                        <a href="#">{rentOptionsDescription}</a>
+                      </h2>
+                      <p className="place-card__type">Apartment</p>
+                    </div>
+                  </article>
+                ))
+                }
 
               </div>
             </section>
@@ -109,5 +134,12 @@ const Main = (props) => {
   );
 };
 
+Main.propTypes = {
+  rentOptionsCount: PropTypes.number.isRequired,
+
+  rentOptionsDescriptions: PropTypes.arrayOf(
+      PropTypes.string
+  ).isRequired
+};
 
 export default Main;
